55186b1d23a923d6292a65cd9eb2e8d3d10237c0
amp221 <amp22one@gmail.com>
2019-05-02 10:54:27 -0400

    Inserted user profile info from user_data.json including estemated semesters left changed user_data to reflect hard coded myPath class selection

--- js/myPath.js
+++ js/myPath.js
@@ -9,26 +9,22 @@ var classKeys = ["COMP1010", "COMP1030L", "MATH1310", "ENGL1010", "SSEL0001", "C
 
 //function that returns the total number of course credits in the course path
 //function that returns the total number of completed credits.
-var courseCredits = 0, userCredits = 0, creditsLeft = 0; 
+var courseCredits = 0, userCredits = 0, creditsLeft = 0, gradSemLeft = 0;
 var popoverText, cID, myID, test;
 
+ //get cs courses
 $.getJSON("data/courses.json", function (csCourses) {
-    console.log(csCourses);
     
-    console.log(csCourses.courses[classKeys[0]]);
-    
-    //get user data
+    //get user_data
     $.getJSON("data/user_data.json", function (user) {
-        console.log(user.user_courses[classKeys[0]]);
-        
         $(document).ready(function () {
-           
+
             /* function that calculates total number of course credits in the course path, 
             total number of users completed credits and the remaining credits */
             function tallyCredits() {
                 for (let index = 0, len = classKeys.length; index < len; index++) {
                     courseCredits += csCourses.courses[classKeys[index]].cCredit;
-                    if (user.user_courses[classKeys[index]] == "completed") 
+                    if (user.user_courses[classKeys[index]] == "completed")
                         userCredits += csCourses.courses[classKeys[index]].cCredit;
                 }
                 creditsLeft = courseCredits - userCredits;
@@ -36,21 +32,20 @@ $.getJSON("data/courses.json", function (csCourses) {
                 console.log(userCredits);
             }
             tallyCredits();
-            
-            /* calculates the remianing number of credits then divides it by 15
-            (we may want to replace user's input/preference if we have time)*/
+
+            /* calculates the remianing number of credits then divides it by 15 and applys 
+            the Math.floor function to round down (we may want to replace user's input/preference if we have time)*/
             function semestersLeft() {
-                creditsLeft = Math.ceil(creditsLeft / 15);
-                console.log(creditsLeft);
+                //gradSemLeft = Math.ceil(creditsLeft / 15);
+                gradSemLeft = Math.floor(creditsLeft / 15);
+                console.log(gradSemLeft);
             }
             semestersLeft();
-            
+
             /* inserts pre/coreq's into popover */
             function getPreCoreq() {
-                
                 for (let index = 0, len = classKeys.length; index < len; index++) {
                     popoverText = "";
-                    
                     preLen = csCourses.courses[classKeys[index]].preReq.length;
                     coLen = csCourses.courses[classKeys[index]].coReq.length;
                     //checking for preReq(s)
@@ -60,7 +55,7 @@ $.getJSON("data/courses.json", function (csCourses) {
                         popoverText += "Prereq:"
                         for (var i = 0; i < preLen; i++) {
                             popoverText += ' ' + csCourses.courses[classKeys[index]].preReq[i] + ",";
-                        }  
+                        }
                     }
                     popoverText += "\n";
                     // checking for coreq(s)
@@ -70,7 +65,7 @@ $.getJSON("data/courses.json", function (csCourses) {
                         popoverText += "\n Coreq:"
                         for (var i = 0; i < coLen; i++) {
                             popoverText += ' ' + csCourses.courses[classKeys[index]].coReq[i] + ", \n";
-                        }  
+                        }
                     }
                     cID = '#';
                     cID += classKeys[index];
@@ -79,14 +74,42 @@ $.getJSON("data/courses.json", function (csCourses) {
             }
             getPreCoreq();
 
-        /* checks for change in course status and prints/returns course number and status.
-         we can use this to update the course status in the future*/
+            /* populates user profile
+
+            "user_info": {
+                "userName": "john_smith@student.uml.edu",
+                "userPassword": "password",
+                "userFirst": "John",
+                "userLast": "Smith",
+                "userMajor": "Computer Science | General",
+                "advisor": "Some Guy",
+                "advisorEmail": "Some_Guy@uml.edu"
+            },
+            <h2 class="student-name">John Smith</h2>
+            <p class="student-major-track">Computer Science | General</p>
+            <p class="student-grad-date">Graduating: May 2023</p>
+            <p class="advisor-name">Advisor: Some Guy</p>
+            <p class="advisor-email">Email: some_guy@uml.edu</p>
+            ************************************************
+            
+            */
+            function getProfile() {
+                $('.student-name').contents().replaceWith(user.user_info.userFirst + " " + user.user_info.userLast);
+                $('.student-major-track').contents().replaceWith(user.user_info.userMajor); 
+                $('.student-grad-date').contents().replaceWith("Estimated Semesters Left:" + " " + gradSemLeft);
+                $('.advisor-name').contents().replaceWith("Advisor: " + user.user_info.advisor);
+                $('.advisor-email').contents().replaceWith("Advisor Email: " + user.user_info.advisorEmail);
+            }
+            getProfile();
+            
+            /* checks for change in course status and prints/returns course number and status.
+             we can use this to update the course status in the future*/
             $("select").change(function () {
                 var selectedStatus = $(this).children("option:selected").val();
                 alert(this.className + " You have selected  " + selectedStatus);
             });
 
-        }); 
+        });
 
     }); // end user_data scope
 
